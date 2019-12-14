ce478c9799970320bc3f637cc40e2845daaa3a95
kouzana fedi <fedi.kouzana1@gmail.com>
2019-12-14 02:31:22 +0100

    Init Grid component
    Component that will manage/display minesweeper's grid

---
+++ src/components/minesweeper/Grid.vue
@@ -0,0 +1,17 @@
+<template>
+  <table>
+    <tr v-for="(rows,i) in minesweeperGrid" :key="i">
+      <td v-for="(cells,j) in rows" :key="j">{{minesweeperGrid[i][j]}}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "Grid",
+  props: ["minesweeperGrid"]
+};
+</script>
+
+<style>
+</style>
