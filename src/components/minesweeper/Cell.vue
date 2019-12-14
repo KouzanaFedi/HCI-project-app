be6255f25643f179ef73c00f2a2f6c64f995af17
kouzana fedi <fedi.kouzana1@gmail.com>
2019-12-14 03:36:31 +0100

    Created Cell component
    Cell component will manage the state of single cell in grid

---
+++ src/components/minesweeper/Cell.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">{{index}}</div>
+</template>
+
+<script>
+export default {
+  name: "Cell",
+  props: ["index"],
+  data() {
+    return {
+      hiden: true
+    };
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  width: 45px;
+  height: 45px;
+
+  text-align: center;
+}
+</style>
--- src/components/minesweeper/Grid.vue
+++ src/components/minesweeper/Grid.vue
@@ -1,17 +1,31 @@
 <template>
   <table>
     <tr v-for="(rows,i) in minesweeperGrid" :key="i">
-      <td v-for="(cells,j) in rows" :key="j">{{minesweeperGrid[i][j]}}</td>
+      <td v-for="(cells,j) in rows" :key="j">
+        <Cell :index="minesweeperGrid[i][j]" />
+      </td>
     </tr>
   </table>
 </template>
 
 <script>
+import Cell from "./Cell";
 export default {
   name: "Grid",
-  props: ["minesweeperGrid"]
+  props: ["minesweeperGrid"],
+  components: {
+    Cell
+  }
 };
 </script>
 
 <style>
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  border: 2px solid black;
+}
+table td {
+  border: 2px solid black;
+}
 </style>
